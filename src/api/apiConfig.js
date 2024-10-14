6bd18eeb4656cc49ba26fd1ca2be81df11ec80ee
Light-royal24 <royallight24@gmail.com>
2024-10-14 04:16:16 +0100

    hide apikey

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,8 @@
+import apikey from "./api-Key";
+
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "23bb226359a7da6096b3ec4213e007de",
+  apiKey: `${apikey}`,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
